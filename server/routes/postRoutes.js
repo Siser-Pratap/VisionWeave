d093c9fdf6f0be60830b0678ebadbca1d8199245
Siser Pratap <siserinsevoc@gmail.com>
2024-09-06 01:04:36 +0530

    Intitalized postRoutes and imageRoutes

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import {v2 as cloudinary} from "cloudinary";
-import {Configuration, OpenAIApi} from "openai";
+// import configuration
 
 import Post from "../models/post.js";
 
 dotenv.config();
 
 const router = express.Router();
+
+
+export default router
