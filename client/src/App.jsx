5090da5cb936c83a5a255af942b9c69bda797e7d
Siser Pratap <siserinsevoc@gmail.com>
2024-09-05 14:07:11 +0530

    Added some features of homepage of image_gen

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import { logo } from './assets';
 import { Home, CreatePost } from './pages';
 
-const App = () => {
+const App = () => (
   <BrowserRouter>
     <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
       <Link to="/">
@@ -20,6 +20,6 @@ const App = () => {
       </Routes>
     </main>
   </BrowserRouter>
-};
+);
 
 export default App;
