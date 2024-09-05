4d498f219773805b95f6a58230532a2445b4c643
Siser Pratap <siserinsevoc@gmail.com>
2024-09-05 23:41:41 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -2,28 +2,48 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./mongodb/connect.js";
-
 import imageRoutes from "./routes/imageRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
 
 
+
+
+
+
+
 dotenv.config();
-
 const app = express();
-
 const port = 8080;
 
+
+
+
+
+
+//middleware middle
 app.use(cors());
 app.use("/api/v1/post", postRoutes);
 app.use("/api/v1/image", imageRoutes);
-
-
 app.use(express.json({limit:"50mb"}));
+
+
+
+
+
+
 
 app.get("/", async(req, res)=>{
     res.send("Hello from Image_Gen");
 });
 
+
+
+
+
+
+
+
+//connecting to database and port
 const startServer = async() => {
     try{
         connectDB(process.env.MONGODB_URL);
@@ -35,8 +55,5 @@ const startServer = async() => {
         console.log(error);
     }
 }
-
-
-
 startServer();
 
