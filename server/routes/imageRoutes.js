7e514261c89fec4dbc0fbe57d31284dacbcfb66c
Siser Pratap <siserinsevoc@gmail.com>
2024-09-06 01:27:24 +0530

    connected to the OpenAi Api

--- server/routes/imageRoutes.js
+++ server/routes/imageRoutes.js
@@ -1,18 +1,18 @@
 import express from "express";
 import * as dotenv from "dotenv";
-import {Configuration, OpenAIApi} from "openai";
+import {OpenAI} from "openai";
 
 
 
 dotenv.config();
-
 const router = express.Router();
 
-const configuration = new Configuration({
+//connecting with openai API
+const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
-})
+});
 
-const openai = new OpenAIApi(configuration);
+
 
 router.route("/").get((req, res)=> {
     res.send("Hello from Image_Gen");
