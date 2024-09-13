f58fb946f08bd944e3957f224d9d4d6fa793992b
Siser Pratap <siserinsevoc@gmail.com>
2024-09-13 23:10:53 +0530

    added backend link

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -49,7 +49,7 @@ const Home = () => {
         
         setLoading(true);
         
-        const response = await fetch("http://localhost:8080/api/v1/post", {
+        const response = await fetch("https://image-gen-rnty.onrender.com/api/v1/post", {
           method:"GET",
           headers:{
             'Content-Type': 'application/json',
