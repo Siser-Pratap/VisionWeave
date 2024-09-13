d1bdcafd813bdfa11750ba0d2e5738455f46a797
Siser Pratap <siserinsevoc@gmail.com>
2024-09-13 14:57:26 +0530

    reintialized createpost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,160 +1,12 @@
-// import React, { useState } from 'react';
-// import { useNavigate } from 'react-router-dom';
-// import OpenAI from "openai";
-// import axios from "axios";
-
-// import { preview } from '../assets';
-// import { getRandomPrompt } from '../utils/index.js';
-// import FormField from '../components/FormField.jsx';
-// import Loader from '../components/Loader.jsx';
-
-// const CreatePost = () => {
-//   const navigate = useNavigate();
-
-//   const [form, setForm] = useState({
-//     name: '',
-//     prompt: '',
-//     url:"",
-//   });
-
-//   const [generatingImg, setGeneratingImg] = useState(false);
-//   const [loading, setLoading] = useState(false);
-
-//   const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
-
-//   const handleSurpriseMe = () => {
-//     const randomPrompt = getRandomPrompt(form.prompt);
-//     setForm({ ...form, prompt: randomPrompt });
-//   };
-
-//   const generateImage = async () => {
-//     console.log(`prompt=${form.prompt}`);
-//     if (form.prompt) {
-//       console.log("running prompt");
-//       try {
-//         setGeneratingImg(true);
-//         // const response = await axios.post("http://localhost:8080/api/v1/image", form.prompt);
-//         const response = await fetch('http://localhost:8080/api/v1/image', {
-//           method: 'POST',
-//           headers: {
-//             'Content-Type': 'application/json',
-//           },
-//           body: JSON.stringify({
-//             prompt: form.prompt,
-//           }),
-//         });
-//         console.log("true");
-//         console.log(response.json());
-//         const image = await response.json();
-//         const url = image.data.data.url;
-        
-//         setForm({ ...form, url:`${url}`});
-//       } catch (err) {
-//         console.log("error occured in createpost");
-//         console.log(err);
-//       } finally {
-//         setGeneratingImg(false);
-//       }
-//     } else {
-//       alert('Please provide proper prompt');
-//     }
-//   };
-
-  
-
-//   const handleSubmit = async (e) => {
-   
-//   };
-
-//   return (
-//     <section className="max-w-7xl mx-auto">
-//       <div>
-//         <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
-//         <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]">Generate an imaginative image through DALL-E AI and share it with the community</p>
-//       </div>
-
-//       <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
-//         <div className="flex flex-col gap-5">
-//           <FormField
-//             labelName="Your Name"
-//             type="text"
-//             name="name"
-//             placeholder="Ex., john doe"
-//             value={form.name}
-//             handleChange={handleChange}
-//           />
-
-//           <FormField
-//             labelName="Prompt"
-//             type="text"
-//             name="prompt"
-//             placeholder="An Impressionist oil painting of sunflowers in a purple vase…"
-//             value={form.prompt}
-//             handleChange={handleChange}
-//             isSurpriseMe
-//             handleSurpriseMe={handleSurpriseMe}
-//           />
-
-//           <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
-//             { form.photo ? (
-//               <img
-//                 src={form.url}
-//                 alt={form.prompt}
-//                 className="w-full h-full object-contain"
-//               />
-//             ) : (
-//               <img
-//                 src={preview}
-//                 alt="preview"
-//                 className="w-9/12 h-9/12 object-contain opacity-40"
-//               />
-//             )}
-
-//             {generatingImg && (
-//               <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
-//                 <Loader />
-//               </div>
-//             )}
-//           </div>
-//         </div>
-
-//         <div className="mt-5 flex gap-5">
-//           <button
-//             type="button"
-//             onClick={generateImage}
-//             className=" text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
-//           >
-//             {generatingImg ? 'Generating...' : 'Generate'}
-//           </button>
-//         </div>
-
-//         <div className="mt-10">
-//           <p className="mt-2 text-[#666e75] text-[14px]">** Once you have created the image you want, you can share it with others in the community **</p>
-//           <button
-//             type="submit"
-//             className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
-//           >
-//             {loading ? 'Sharing...' : 'Share with the Community'}
-//           </button>
-//         </div>
-//       </form>
-//     </section>
-//   );
-// };
-
-// export default CreatePost;
-
-
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-
-
 import { preview } from '../assets';
 import { getRandomPrompt } from '../utils';
 import { FormField, Loader } from '../components';
 
 const CreatePost = () => {
+
   const navigate = useNavigate();
 
   const [form, setForm] = useState({
@@ -164,6 +16,7 @@ const CreatePost = () => {
   });
 
   const [generatingImg, setGeneratingImg] = useState(false);
+
   const [loading, setLoading] = useState(false);
 
   const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
@@ -173,62 +26,28 @@ const CreatePost = () => {
     setForm({ ...form, prompt: randomPrompt });
   };
 
-  
-
-
-  
-  
-
-
-
-
   const generateImage = async () => {
-  //   if (form.prompt) {
-  //     try {
-  //       setGeneratingImg(true);
-  //       const response = await fetch('http://localhost:8080/api/v1/image', {
-  //         method: 'POST',
-  //         headers: {
-  //           'Content-Type': 'application/json',
-  //         },
-  //         body: JSON.stringify({
-  //           prompt: form.prompt,
-  //         }),
-  //       });
+    const options = {
+      method: 'POST',
+      url: 'https://api.getimg.ai/v1/flux-schnell/text-to-image',
+      headers: {
+        accept: 'application/json',
+        'content-type': 'application/json',
+        authorization: 'Bearer key-4SE8wefN1bTDZFuY8iGbbAvk7YrSanclKRb0Wmyyz4TZm5fChAvnEJpVv6lzGrvsRab4FnbWOKNJOdsZDnyuWP7w1Iz5poL7'
+      },
+      data: {prompt: `${form.prompt}`, response_format: 'url'}
+    };
 
-  //       const data = await response.json();
-  //       setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
-        
-
-
-  //     } catch (err) {
-  //       alert(err);
-  //     } finally {
-  //       setGeneratingImg(false);
-  //     }
-  //   } else {
-  //     alert('Please provide proper prompt');
-  //   }
-
-  const options = {
-    method: 'POST',
-    url: 'https://api.getimg.ai/v1/flux-schnell/text-to-image',
-    headers: {
-      accept: 'application/json',
-      'content-type': 'application/json',
-      authorization: 'Bearer key-4SE8wefN1bTDZFuY8iGbbAvk7YrSanclKRb0Wmyyz4TZm5fChAvnEJpVv6lzGrvsRab4FnbWOKNJOdsZDnyuWP7w1Iz5poL7'
-    },
-    data: {prompt: `${form.prompt}`, response_format: 'url'}
-  };
-
-  try {
-    const response = await axios.request(options);
-    console.log(response.data);
-    setForm({ ...form, url: response.data.url });
-    
-  } catch (error) {
-    console.log(error.message);
-  }
+    try {
+      setGeneratingImg(true);
+      const response = await axios.request(options);
+      console.log(response.data);
+      setForm({ ...form, url: response.data.url });
+      setGeneratingImg(false);
+      
+    } catch (error) {
+      console.log(error.message);
+    }
 
 
 
@@ -236,30 +55,7 @@ const CreatePost = () => {
   };
 
   const handleSubmit = async (e) => {
-    // e.preventDefault();
-
-    // if (form.prompt && form.photo) {
-    //   setLoading(true);
-    //   try {
-    //     const response = await fetch('https://dalle-arbb.onrender.com/api/v1/post', {
-    //       method: 'POST',
-    //       headers: {
-    //         'Content-Type': 'application/json',
-    //       },
-    //       body: JSON.stringify({ ...form }),
-    //     });
-
-    //     await response.json();
-    //     alert('Success');
-    //     navigate('/');
-    //   } catch (err) {
-    //     alert(err);
-    //   } finally {
-    //     setLoading(false);
-    //   }
-    // } else {
-    //   alert('Please generate an image with proper details');
-    // }
+   
   };
 
   return (
@@ -301,7 +97,7 @@ const CreatePost = () => {
             ) : (
               <img
                 src={preview}
-                alt="preview"
+                alt=""
                 className="w-9/12 h-9/12 object-contain opacity-40"
               />
             )}
