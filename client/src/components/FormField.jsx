48de75b20fe16835b7abcb56de5c47bc2d7b50fe
Siser Pratap <siserinsevoc@gmail.com>
2024-11-20 23:05:09 +0530

    updated settings

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -14,7 +14,7 @@ const FormField = ({
     <div className="flex items-center gap-2 mb-2 ml-2">
       <label
         htmlFor={name}
-        className="block text-sm mx-2 sm:mx-2 font-medium text-white/70"
+        className="block text-sm mx-2 sm:mx-2 font-medium text-white/80"
       >
         {labelName}
       </label>
@@ -22,7 +22,7 @@ const FormField = ({
         <button
           type="button"
           onClick={handleSurpriseMe}
-          className="font-semibold text-xs bg-[#EcECF1] py-1 px-2 rounded-[5px] text-black"
+          className="font-semibold text-xs bg-[#EcECF1] py-1 px-2 rounded-[5px] text-white/80 bg-inherit hover:border-secondary border-[1px]"
         >
           Surprise me
         </button>
@@ -32,7 +32,7 @@ const FormField = ({
       type={type}
       id={name}
       name={name}
-      className="bg-gray-50 mx-4 sm:mx-2 border-[2px] sm:border-[2px] border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#6469ff] focus:border-secondary outline-none block w-[90%]  sm:w-full p-3"
+      className="bg-inherit border-inherit mx-4 sm:mx-2 border-[0px] text-white/80 sm:border-[0px] hover:border-[1px] hover:border-secondary text-sm rounded-lg focus:ring-[#6469ff] focus:border-secondary outline-none block w-[90%]  sm:w-full p-3"
       placeholder={placeholder}
       value={value}
       onChange={handleChange}
